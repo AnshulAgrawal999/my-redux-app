c54875ace50beb0c9f75659c157799044d86784c
anshul <anshulagr799@gmail.com>
2024-12-09 13:02:22 +0530

    react app setup

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import store from './app/store'
+import store from './app/store.js'
 import { Provider } from 'react-redux'
 
 createRoot(document.get<StrictMode>ElementById('root')).render(
   <Provider store={store}>
     <App />
-    </Provider>
+  </Provider> 
 )
