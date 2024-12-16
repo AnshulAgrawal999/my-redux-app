7bd4582f76ebd54d1788fd30ad10fe716ff21df5
anshul <anshulagr799@gmail.com>
2024-12-16 11:52:34 +0530

    add reset to 0

--- src/pages/CounterPage.jsx
+++ src/pages/CounterPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement, fetchCountFromAPI } from '../redux/counterSlice';
+import { increment, decrement, incrementByAmount , reset , fetchCountFromAPI } from '../redux/counterSlice';
 
 function CounterPage() {
     const count = useSelector((state) => state.counter.value);
@@ -20,9 +20,13 @@ function CounterPage() {
 
             <p>Status: {status}</p>
 
-            <button onClick={() => dispatch(increment(1))}>Increment</button>
+            <button onClick={() => dispatch(increment(1))}>Increment+</button>
 
-            <button onClick={() => dispatch(decrement(1))}>Decrement</button>
+            <button onClick={() => dispatch(decrement(1))}>Decrement-</button>
+
+            <button onClick={() => dispatch( incrementByAmount(2) )}> Increment by 2+ </button>
+
+            <button onClick={() => dispatch( reset() )}> Reset to 0 </button>
             
             <button onClick={() => dispatch(fetchCountFromAPI())}>
                 Fetch Count from API
