e6ddd663675cfec4923123186d02a3a635dd254f
anshul <anshulagr799@gmail.com>
2024-12-16 12:10:48 +0530

    import lodash

--- src/pages/PackagesList.jsx
+++ src/pages/PackagesList.jsx
@@ -1,4 +1,21 @@
-import React from 'react'  ;
+import React, { useState, useEffect, useReducer } from 'react'
+import {
+    Box, Text, Spacer, HStack, Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    Button, Spinner, Tag, Input, InputGroup, InputRightElement, InputLeftElement, Table,
+    Tbody,
+    Tr,
+    Td,
+    Select,
+    MenuItemOption, MenuOptionGroup, MenuDivider, Tooltip, Thead, Th
+} from '@chakra-ui/react';
+
+import { AddIcon, ChevronDownIcon, SearchIcon, DeleteIcon, CloseIcon, ArrowUpDownIcon } from '@chakra-ui/icons'
+
+import _ from 'lodash';
+
 
 function PackagesList() {
     return (
