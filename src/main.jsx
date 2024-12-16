bd2f01720a4faea0d258e25cee1414b2f67d4b67
anshul <anshulagr799@gmail.com>
2024-12-16 09:56:27 +0530

    downgrade versions to ensure compatibility

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom';
 
 import App from './App.jsx'
 
@@ -6,8 +6,62 @@ import { Provider } from 'react-redux'
 
 import store from './redux/store.js'
 
-createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider> 
-)
+import { ChakraProvider} from "@chakra-ui/react";
+
+import { extendTheme } from "@chakra-ui/react";
+
+// Let's say you want to add custom colors
+const customTheme = extendTheme({
+	styles: {
+		global: {
+			"h1, .h1, h2, .h2,h3, .h3": {
+				marginTop: "25px",
+				marginBottom: "12.5px",
+			},
+
+			"h4, .h4,h5, .h5, h6, .h6": {
+				marginTop: "12.5px",
+				marginBottom: "12.5px",
+			},
+
+			hr: {
+				marginTop: "5px",
+				marginBottom: "25px",
+			},
+
+			a: {
+				color: "#2EB398",
+			},
+
+			"h1, .h1": { fontSize: "36px" },
+			"h2, .h2": { fontSize: "30px" },
+			"h3, .h3": { fontSize: "24px" },
+			"h4, .h4": { fontSize: "18px" },
+			"h5, .h5": { fontSize: "14px" },
+			"h6, .h6": { fontSize: "12px" },
+
+			img: {
+				display: "initial"
+			},
+
+			"ul, ol": {
+				paddingInlineStart: '40px'
+			},
+
+			body: {
+				lineHeight: 1.78571
+			}
+
+		},
+	},
+});
+
+
+ReactDOM.render(
+	<ChakraProvider>
+	  <Provider store={store}>
+		<App />
+	  </Provider>
+	</ChakraProvider>,
+	document.getElementById('root')
+  );
